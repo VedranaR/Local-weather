b3e48ee5e81deccad4b89db4b6ef916b995fa336
Vedrana <vedrana_ravlic16@yahoo.com>
2019-01-02 21:38:42 +0100

    changed ui cathegories for api call

--- weather.js
+++ weather.js
@@ -1,7 +1,15 @@
 class Weather {
-  constructor(city, state) {
+  constructor(city, country) {
     this.apiKey = "60948ba4157b468519c8ac0fc87c3121";
     this.city = city;
-    this.state = state;
+    this.country = country;
+  }
+
+  async getWeather() {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${
+        this.country
+      }&appid=${this.apiKey}`
+    );
   }
 }
