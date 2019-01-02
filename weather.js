5e024c4088b0566b3c7029e4f6bf0ad31091569d
Vedrana <vedrana_ravlic16@yahoo.com>
2019-01-02 22:06:18 +0100

    defined weather class

--- weather.js
+++ weather.js
@@ -5,11 +5,22 @@ class Weather {
     this.country = country;
   }
 
+  //Fetch the API data
   async getWeather() {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${
         this.country
       }&appid=${this.apiKey}`
     );
+
+    const responseData = await response.json;
+
+    return responseData;
+  }
+
+  //Change the location
+  changeLocation(city, country) {
+    this.city = city;
+    this.country = country;
   }
 }
