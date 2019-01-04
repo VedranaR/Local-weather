85a57045576be688b4b90159e554e61b20380684
Vedrana <vedrana_ravlic16@yahoo.com>
2019-01-04 21:54:01 +0100

    added chosing location func

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ const weather = new Weather(city, country);
 const ui = new UI();
 
 //Get the weather info on DOM load
-
 document.addEventListener("DOMContentLoaded", getWeather);
+
+//Change location event
+document.getElementById("change-btn").addEventListener("click", e => {
+  const city = document.getElementById("city").value;
+  const country = document.getElementById("country").value;
+
+  weather.changeLocation(city, country);
+
+  //Get and display weather
+  getWeather();
+
+  //Close modal window
+  $("#locModal").modal("hide");
+});
 
 function getWeather() {
   weather
