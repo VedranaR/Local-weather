e34954a3f48093303f35fa7d4bb5c9293c28d55d
Vedrana <vedrana_ravlic16@yahoo.com>
2019-01-04 22:13:02 +0100

    checking4 & fetching the data from loc storage

--- storage.js
+++ storage.js
@@ -6,6 +6,26 @@ class Storage {
     this.defaultCountry = "CRO";
   }
 
+  getLocationData() {
+    //check if anything's stored for the city & country
+    if (localStorage.getItem("city") === null) {
+      this.city = this.defaultCity;
+    } else {
+      this.city = localStorage.getItem("city");
+    }
+
+    if (localStorage.getItem("country") === null) {
+      this.country = this.defaultCountry;
+    } else {
+      this.country = localStorage.getItem("country");
+    }
+
+    return {
+      city: this.city,
+      country: this.country
+    };
+  }
+
   setLocationData(city, country) {
     localStorage.setItem("city", city);
     localStorage.setItem("country", country);
